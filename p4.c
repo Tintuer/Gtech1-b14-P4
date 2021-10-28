533c4e3dde64b1d09b15245341a4795833b33c21
Tintuer <92401751+Tintuer@users.noreply.github.com>
2021-10-28 23:01:46 +0200

    Update p4

--- p4.c
+++ p4.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
+#define NBL = 6
+#define NBC = 7
 #define tokken "OX"
-int col;
 
-char tab[6][7];
+char tab[NBL][NBC];
 
 void init(void){ // initialisation du tableau
-  for(int l=0; l<6; l++)
+  for(int l=0; l<NBL; l++)
     {
-      for(int c=0; c<7; c++)
+      for(int c=0; c<NBC; c++)
 	{
 	  tab[l][c] = '.' ;
 	}
@@ -19,24 +19,14 @@ void init(void){ // initialisation du tableau
 }
 
 void showtab(void){ // affichage du tableau
-  for(int l=0; l<6; l++)
+  for(int l=0; l<NBL; l++)
     {
       printf("\n");
-      for(int c=0; c<7; c++)
+      for(int c=0; c<NBC; c++)
 	{
 	  printf("%c ",tab[l][c]);
 	}
     }
-}
-
-int valid(int number)
-{
-  if ( (number < 0) || (number > 6) )
-    printf("SALE ENCULER");
-    return 0;
-  if (tab[6-1][number] != '.')
-    return 0;
-  return 1;
 }
 
 int ask(void){ // demande de réponse du joueur
@@ -48,7 +38,7 @@ int ask(void){ // demande de réponse du joueur
 
 int bottom(int col){ // vérification de la place sur une colonne
   int line = 0;
-  while(line <= 5 || line == 1){
+  while(line < NBL ){
     if(tab[line][col]  == '.'){
       line++;
     }
@@ -61,22 +51,84 @@ int bottom(int col){ // vérification de la place sur une colonne
 
 void play(int player,int col){ // place des tokkens dans le jeu
   int line = bottom(col);
+  line = error(line);
   tab[line][col]= tokken[player];
 }
 
+int error(int col){
+    int line;
+    line = bottom(col);
+    while(line == -1){
+        printf("%c","Colonne pleine. \nVeuillez choisir une colonne valide :")
+        scanf("%d",col);
+        line = bottom(col);
+    }
+    return line;
+}
+
+int winhor(void){
+    for(int i = 0; i<NBL; i++){
+        for(int j = 0; j<NBC-3; j++){
+            if(tab[i][j] == tokken[player] && tab[i][j] == tab[i][j+1] && tab[i][j] == tab[i][j+2] && tab[i][j] == tab[i][j+3]){
+                printf("%c","Bien joué tu as gagné")
+                return 1;
+            }
+        }
+    }
+}
+
+int winver(void){
+    for(int i = 0; i<NBL-3; i++){
+        for(int j = 0; j<NBC; j++){
+            if(tab[i][j] == tokken[player] && tab[i][j] == tab[i+1][j] && tab[i][j] == tab[i+2][j] && tab[i][j] == tab[i+3][j]){
+                printf("%c","Bien joué tu as gagné")
+                return 1;
+            }
+        }
+    }
+}
+
+int windiag1(void){
+    for(int i = 0; i<NBL-3; i++){
+        for(int j = 0; j<NBC-3; j++){
+            if(tab[i][j] == tokken[player] && tab[i][j] == tab[i+1][j+1] && tab[i][j] == tab[i+2][j+2] && tab[i][j] == tab[i+3][j+3]){
+                printf("%c","Bien joué tu as gagné")
+                return 1;
+            }
+        }
+    }
+}
+
+int windiag2(void){
+    for(int i = 3; i<NBL; i++){
+        for(int j = 3; j<NBC; j++){
+            if(tab[i][j] == tokken[player] && tab[i][j] == tab[i+1][j-1] && tab[i][j] == tab[i+2][j-2] && tab[i][j] == tab[i+3][j-3]){
+                printf("%c","Bien joué tu as gagné")
+                return 1;
+            }
+        }
+    }
+}
 
 int main(void){ // fonction principale du jeu
-  int counter=0;
-  int tokkenmax=6*7;
+  int counter = 0;
+  int tokkenmax  =6*7;
   int player = 0;
+  int victory = 0;
   init();
-  while (counter<tokkenmax){
+  while (counter<=tokkenmax && victory == 0){
     showtab();
     play(player,ask());
+    victory = winhor();
+    victory = winver();
+    victory = windiag1();
+    victory = windiag2();
     counter ++;
     player = !player;
   }
-  printf("draw");
+  if(counter > tokkenmax){
+      printf("%c","draw");
+  }
   return 0;
 
 }
